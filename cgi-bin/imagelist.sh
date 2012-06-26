0de87d9d3fa51c226ced428a93c84b579abc5b97
root <root@interactive.(none)>
2012-06-26 10:53:18 +0000

    Limit number of shown images

--- cgi-bin/imagelist.sh
+++ cgi-bin/imagelist.sh
@@ -12,3 +12,7 @@ echo "Content-Type: application/json
 $s
 "
 exit 0
+
+# One day we may want to get only x newest files
+#[ "$1" ] && limit=$1 || limit=100
+#for f in $(stat -c "%Y %n" img/*.png | sort -rn | tail -n $limit | cut -d ' ' -f 1 --complement); do
